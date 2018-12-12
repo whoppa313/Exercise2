7655ca4358d06debff9bbed0f99f41d46e89c0ee
Victor_506 <viho98001@utb.vaxjo.se>
2018-12-12 14:34:12 +0100

    Exercise2

---
+++ src/main/java/Lexicon_Java_victor/Exercise2/App.java
@@ -0,0 +1,33 @@
+package Lexicon_Java_victor.Exercise2;
+
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       
+    	Scanner in = new Scanner(System.in);
+    	
+    	int a = in.nextInt();
+    	
+    	if (a % 4 != 0)
+    	System.out.println(a + " är inte skottår");
+    	
+   
+    			
+    	else if (a % 4 == 0 )
+        	
+        System.out.println(a + " är skottår");
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    }
+}
